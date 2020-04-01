da69c805c7c766c45f0a92ee58b7cae02e6c1937
Andriy Tykhonov <atykhonov@gmail.com>
2020-04-01 20:09:41 +0300

    Create issue type `Question`.

--- tools/jira-configuration.py
+++ tools/jira-configuration.py
@@ -12,8 +12,10 @@ from requests.auth import HTTPBasicAuth
 import utils
 
 
+QUESTION_ISSUE_TYPE_NAME = 'Question'
 FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
 JIRA_SERVER_URL = os.environ['JIRA_SERVER_URL']
+JIRA_API_URL =  f'{JIRA_SERVER_URL}/rest/api/3'
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
 [
  logging.getLogger(p).setLevel(logging.ERROR)
@@ -22,6 +24,13 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
 logger = logging.getLogger()
 jira = utils.get_jira()
 fake = Faker()
+auth = HTTPBasicAuth(
+    os.environ['JIRA_USER_EMAIL'], os.environ['JIRA_API_TOKEN'])
+
+headers = {
+   'Accept': 'application/json',
+    'Content-Type': 'application/json',
+}
 
 
 def create_project(key, name):
@@ -43,16 +52,6 @@ def create_project(key, name):
         logger.info(f'Project "{name}" already exists. Skipping...')
         return project
 
-    url = '/rest/api/3/project'
-
-    auth = HTTPBasicAuth(
-        os.environ['JIRA_USER_EMAIL'], os.environ['JIRA_API_TOKEN'])
-
-    headers = {
-       'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    }
-
     payload = json.dumps({
         'name': name,
         'projectTypeKey': 'software',
@@ -61,9 +60,8 @@ def create_project(key, name):
         'leadAccountId': jira.myself()['accountId'],
     })
 
-    response = requests.request(
-       'POST',
-       '{}{}'.format(JIRA_SERVER_URL, url),
+    response = requests.post(
+       f'{JIRA_API_URL}/project',
        data=payload,
        headers=headers,
        auth=auth
@@ -90,13 +88,33 @@ def create_issue(project_key, summary):
         logger.exception('Error occurred while creating an issue')
 
 
+def create_issue_link_type(name, outward, inward):
+    payload = json.dumps({
+        'name': name,
+        'outward': outward,
+        'inward': inward,
+    })
+
+    response = requests.post(
+       f'{JIRA_API_URL}/issueLinkType',
+       data=payload,
+       headers=headers,
+       auth=auth
+    )
+
+    if not response.ok:
+        response.raise_for_status()
+    else:
+        logger.info(f'Issue link type "{name}" successfully created')
+
+
 if __name__ == "__main__":
     person_project_key = 'PERSON'
     project = create_project(person_project_key, 'Persons')
     if project:
-        create_issue(person_project_key, fake.sentence())
-        create_issue(person_project_key, fake.sentence())
-        create_issue(person_project_key, fake.sentence())
+        create_issue(person_project_key, fake.name())
+        create_issue(person_project_key, fake.name())
+        create_issue(person_project_key, fake.name())
 
     incident_project_key = 'INCIDENT'
     project = create_project(incident_project_key, 'Incidents')
@@ -111,3 +129,15 @@ if __name__ == "__main__":
         create_issue(question_project_key, fake.sentence())
         create_issue(question_project_key, fake.sentence())
         create_issue(question_project_key, fake.sentence())
+
+    for issue_link_type in jira.issue_link_types():
+        if issue_link_type.name == QUESTION_ISSUE_TYPE_NAME:
+            msg = (
+                f'Issue link type "{QUESTION_ISSUE_TYPE_NAME}" '
+                f'already exists. Skipping...'
+            )
+            logger.info(msg)
+            break
+    else:
+        create_issue_link_type(
+            QUESTION_ISSUE_TYPE_NAME, 'has question', 'is question of')
