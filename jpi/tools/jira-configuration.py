fd06e1d269c283e23f675ecdb782fd86ecda88f2
Andriy Tykhonov <atykhonov@gmail.com>
2020-04-11 16:54:56 +0300

    Move the settings from `jira-configuration.py` to `settings.py`.

--- jpi/tools/jira-configuration.py
+++ jpi/tools/jira-configuration.py
@@ -8,22 +8,11 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 
-from jpi import utils
+from jpi import settings, utils
 
 
-QUESTION_ISSUE_TYPE_NAME = "Question"
-INCIDENT_MANAGER_ISSUE_TYPE_NAME = "Incident Manager"
-TIMELINE_ISSUE_TYPE_NAME = "Timeline"
-STAKEHOLDER_ISSUE_TYPE_NAME = "Stakeholder"
-INCIDENT_PROJECT_KEY = os.environ["INCIDENT_PROJECT_KEY"]
-PERSON_PROJECT_KEY = os.environ["PERSON_PROJECT_KEY"]
-TIMELINE_PROJECT_KEY = os.environ["TIMELINE_PROJECT_KEY"]
-QUESTION_PROJECT_KEY = os.environ["QUESTION_PROJECT_KEY"]
-PAGERDUTY_USER_NAME = os.environ["PAGERDUTY_USER_NAME"]
-FORMAT = "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
-JIRA_SERVER_URL = os.environ["JIRA_SERVER_URL"]
-JIRA_API_URL = f"{JIRA_SERVER_URL}/rest/api/3"
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
+logging.basicConfig(
+    stream=sys.stdout, level=logging.DEBUG, format=settings.LOGGING_FORMAT)
 [
     logging.getLogger(p).setLevel(logging.ERROR)
     for p in ["faker.factory", "urllib3"]
@@ -31,9 +20,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
 logger = logging.getLogger()
 jira = utils.get_jira()
 fake = Faker()
-auth = HTTPBasicAuth(
-    os.environ["JIRA_USER_EMAIL"], os.environ["JIRA_API_TOKEN"]
-)
+auth = HTTPBasicAuth(settings.JIRA_USER_EMAIL, settings.JIRA_API_TOKEN)
 
 headers = {"Accept": "application/json", "Content-Type": "application/json"}
 
@@ -69,7 +56,10 @@ def create_project(key, name):
     )
 
     response = requests.post(
-        f"{JIRA_API_URL}/project", data=payload, headers=headers, auth=auth
+        f"{settings.JIRA_API_URL}/project",
+        data=payload,
+        headers=headers,
+        auth=auth
     )
 
     if not response.ok:
@@ -97,7 +87,7 @@ def create_issue_link_type(name, outward, inward):
     payload = json.dumps({"name": name, "outward": outward, "inward": inward})
 
     response = requests.post(
-        f"{JIRA_API_URL}/issueLinkType",
+        f"{settings.JIRA_API_URL}/issueLinkType",
         data=payload,
         headers=headers,
         auth=auth,
@@ -110,96 +100,89 @@ def create_issue_link_type(name, outward, inward):
 
 
 if __name__ == "__main__":
-    project = create_project(PERSON_PROJECT_KEY, "Persons")
+    project = create_project(settings.PERSON_PROJECT_KEY, "Persons")
     if project:
-        create_issue(PERSON_PROJECT_KEY, fake.name())
-        create_issue(PERSON_PROJECT_KEY, fake.name())
-        create_issue(PERSON_PROJECT_KEY, fake.name())
+        create_issue(settings.PERSON_PROJECT_KEY, fake.name())
+        create_issue(settings.PERSON_PROJECT_KEY, fake.name())
+        create_issue(settings.PERSON_PROJECT_KEY, fake.name())
 
-    project = create_project(INCIDENT_PROJECT_KEY, "Incidents")
+    project = create_project(settings.INCIDENT_PROJECT_KEY, "Incidents")
     if project:
-        create_issue(INCIDENT_PROJECT_KEY, fake.sentence())
-        create_issue(INCIDENT_PROJECT_KEY, fake.sentence())
-        create_issue(INCIDENT_PROJECT_KEY, fake.sentence())
+        create_issue(settings.INCIDENT_PROJECT_KEY, fake.sentence())
+        create_issue(settings.INCIDENT_PROJECT_KEY, fake.sentence())
+        create_issue(settings.INCIDENT_PROJECT_KEY, fake.sentence())
 
-    project = create_project(QUESTION_PROJECT_KEY, "Questions")
+    project = create_project(settings.QUESTION_PROJECT_KEY, "Questions")
     if project:
-        create_issue(QUESTION_PROJECT_KEY, fake.sentence())
-        create_issue(QUESTION_PROJECT_KEY, fake.sentence())
-        create_issue(QUESTION_PROJECT_KEY, fake.sentence())
+        create_issue(settings.QUESTION_PROJECT_KEY, fake.sentence())
+        create_issue(settings.QUESTION_PROJECT_KEY, fake.sentence())
+        create_issue(settings.QUESTION_PROJECT_KEY, fake.sentence())
 
-    project = create_project(TIMELINE_PROJECT_KEY, "Timeline")
+    project = create_project(settings.TIMELINE_PROJECT_KEY, "Timeline")
     if project:
-        create_issue(TIMELINE_PROJECT_KEY, fake.sentence())
-        create_issue(TIMELINE_PROJECT_KEY, fake.sentence())
-        create_issue(TIMELINE_PROJECT_KEY, fake.sentence())
+        create_issue(settings.TIMELINE_PROJECT_KEY, fake.sentence())
+        create_issue(settings.TIMELINE_PROJECT_KEY, fake.sentence())
+        create_issue(settings.TIMELINE_PROJECT_KEY, fake.sentence())
 
     for issue_link_type in jira.issue_link_types():
-        if issue_link_type.name == QUESTION_ISSUE_TYPE_NAME:
+        if issue_link_type.name == settings.QUESTION_ISSUE_TYPE_NAME:
             msg = (
-                f'Issue link type "{QUESTION_ISSUE_TYPE_NAME}" '
+                f'Issue link type "{settings.QUESTION_ISSUE_TYPE_NAME}" '
                 f"already exists. Skipping..."
             )
             logger.info(msg)
             break
     else:
         create_issue_link_type(
-            QUESTION_ISSUE_TYPE_NAME, "has question", "is question of"
+            settings.QUESTION_ISSUE_TYPE_NAME, "has question", "is question of"
         )
 
     for issue_link_type in jira.issue_link_types():
-        if issue_link_type.name == INCIDENT_MANAGER_ISSUE_TYPE_NAME:
-            msg = (
-                f'Issue link type "{INCIDENT_MANAGER_ISSUE_TYPE_NAME}" '
-                f"already exists. Skipping..."
-            )
-            logger.info(msg)
+        if issue_link_type.name == settings.INCIDENT_MANAGER_ISSUE_TYPE_NAME:
+            msg = 'Issue link type "{}" already exists. Skipping...'
+            logger.info(msg.format(settings.INCIDENT_MANAGER_ISSUE_TYPE_NAME))
             break
     else:
         create_issue_link_type(
-            INCIDENT_MANAGER_ISSUE_TYPE_NAME,
+            settings.INCIDENT_MANAGER_ISSUE_TYPE_NAME,
             "has incident manager",
             "is incident manager of",
         )
 
-    persons = jira.search_issues(
-        f'project={PERSON_PROJECT_KEY} and summary~"{PAGERDUTY_USER_NAME}"'
-    )
+    query = 'project={} and summary~"{}"'.format(
+        settings.PERSON_PROJECT_KEY, settings.PAGERDUTY_USER_NAME)
+    persons = jira.search_issues(query)
     if persons:
-        logger.info(
-            f'Person "{PAGERDUTY_USER_NAME}" already exists. Skipping...'
-        )
+        msg = 'Person "{}" already exists. Skipping...'
+        logger.info(msg.format(settings.PAGERDUTY_USER_NAME))
     else:
         issue_dict = {
-            "project": {"key": PERSON_PROJECT_KEY},
-            "summary": PAGERDUTY_USER_NAME,
+            "project": {"key": settings.PERSON_PROJECT_KEY},
+            "summary": settings.PAGERDUTY_USER_NAME,
             "issuetype": {"name": "Story"},
         }
         jira.create_issue(fields=issue_dict)
-        logger.info(f'Person "{PAGERDUTY_USER_NAME}" successfully created')
+        logger.info(
+            f'Person "{settings.PAGERDUTY_USER_NAME}" successfully created')
 
     for issue_link_type in jira.issue_link_types():
-        if issue_link_type.name == TIMELINE_ISSUE_TYPE_NAME:
-            msg = (
-                f'Issue link type "{INCIDENT_MANAGER_ISSUE_TYPE_NAME}" '
-                f"already exists. Skipping..."
-            )
-            logger.info(msg)
+        if issue_link_type.name == settings.TIMELINE_ISSUE_TYPE_NAME:
+            msg = 'Issue link type "{}" already exists. Skipping...'
+            logger.info(msg.format(settings.INCIDENT_MANAGER_ISSUE_TYPE_NAME))
             break
     else:
         create_issue_link_type(
-            TIMELINE_ISSUE_TYPE_NAME, "has timeline", "is timeline of"
+            settings.TIMELINE_ISSUE_TYPE_NAME, "has timeline", "is timeline of"
         )
 
     for issue_link_type in jira.issue_link_types():
-        if issue_link_type.name == STAKEHOLDER_ISSUE_TYPE_NAME:
-            msg = (
-                f'Issue link type "{STAKEHOLDER_ISSUE_TYPE_NAME}" '
-                f"already exists. Skipping..."
-            )
-            logger.info(msg)
+        if issue_link_type.name == settings.STAKEHOLDER_ISSUE_TYPE_NAME:
+            msg = 'Issue link type "{}" already exists. Skipping...'
+            logger.info(msg.format(settings.STAKEHOLDER_ISSUE_TYPE_NAME))
             break
     else:
         create_issue_link_type(
-            STAKEHOLDER_ISSUE_TYPE_NAME, "has stakeholder", "is stakeholder of"
+            settings.STAKEHOLDER_ISSUE_TYPE_NAME,
+            "has stakeholder",
+            "is stakeholder of"
         )
