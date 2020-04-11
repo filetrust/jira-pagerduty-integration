ff491256648ed64212df4145370304599a748ea1
Andriy Tykhonov <atykhonov@gmail.com>
2020-04-11 21:44:10 +0300

    Create timeline issue for each log entry.
    Create timeline issue for each log entry (not only of type
    `status_update_log_entry`).

--- jpi/polling.py
+++ jpi/polling.py
@@ -14,11 +14,57 @@ logger.setLevel(logging.INFO)
 TIMELINE_PROJECT_KEY = os.environ["TIMELINE_PROJECT_KEY"]
 
 
+def handle_log_entry(log_entry):
+    jira = utils.get_jira()
+    logger.info(
+        "[{}] New status update found".format(log_entry["id"])
+    )
+    issue_key = db.get_issue_key_by_incident_id(
+        log_entry["incident"]["id"]
+    )
+    if issue_key:
+        logger.info(
+            "[{}] Related issue found: {}".format(
+                log_entry["id"], issue_key
+            )
+        )
+        try:
+            jira.issue(issue_key)
+        except JIRAError:
+            msg = "[{}] Error occurred while retrieving Jira issue"
+            logger.exception(msg.format(log_entry["id"]))
+            return
+
+        issue_dict = {
+            "project": {"key": os.environ["TIMELINE_PROJECT_KEY"]},
+            "summary": log_entry["summary"],
+            "issuetype": {"name": "Bug"},
+        }
+        try:
+            timeline_issue = jira.create_issue(fields=issue_dict)
+            logger.info(
+                'Timeline issue "{}" successfully created'.format(
+                    timeline_issue.key
+                )
+            )
+            utils.link_issue(
+                timeline_issue.key, issue_key, "has timeline"
+            )
+        except JIRAError:
+            msg = "[{}] Error creating timeline link to Jira issue {}"
+            logger.exception(msg.format(issue_key))
+            return
+        db.put_log_entry(log_entry["id"])
+    else:
+        logger.info(
+            "[{}] Issue key not found".format(log_entry["id"])
+        )
+
+
 def polling_handler(event, context):
     result = {"ok": True}
     timeline = 0
     pagerduty = utils.get_pagerduty()
-    jira = utils.get_jira()
     polling_timestamp = db.last_polling_timestamp()
     if not polling_timestamp:
         poll_past_hours = int(os.environ.get("LOG_ENTRIES_POLL_PAST_HOURS"))
@@ -47,60 +93,9 @@ def polling_handler(event, context):
                 # Usually should not happens as far as we read items from
                 # our last polling call
                 msg = "[{}] Existing log entry found. Skipping..."
-                msg = msg.format(log_entry["id"])
-                logger.info(msg)
+                logger.info(msg.format(log_entry["id"]))
                 continue
-            if log_entry["type"] == "status_update_log_entry":
-                logger.info(
-                    "[{}] New status update found".format(log_entry["id"])
-                )
-                issue_key = db.get_issue_key_by_incident_id(
-                    log_entry["incident"]["id"]
-                )
-                if issue_key:
-                    logger.info(
-                        "[{}] Related issue found: {}".format(
-                            log_entry["id"], issue_key
-                        )
-                    )
-                    try:
-                        jira.issue(issue_key)
-                    except JIRAError:
-                        msg = "[{}] Error occurred while retrieving Jira issue"
-                        msg = msg.format(log_entry["id"])
-                        logger.exception(msg)
-                        result["ok"] = False
-                        result["error"] = msg
-                        continue
-
-                    issue_dict = {
-                        "project": {"key": os.environ["TIMELINE_PROJECT_KEY"]},
-                        "summary": log_entry["message"],
-                        "issuetype": {"name": "Bug"},
-                    }
-                    try:
-                        timeline_issue = jira.create_issue(fields=issue_dict)
-                        logger.info(
-                            'Timeline issue "{}" successfully created'.format(
-                                timeline_issue.key
-                            )
-                        )
-                        utils.link_issue(
-                            timeline_issue.key, issue_key, "has timeline"
-                        )
-                        timeline += 1
-                    except JIRAError:
-                        msg = "[{}] Error creating timeline link to JIRA {}"
-                        msg = msg.format(issue_key)
-                        logger.exception(msg)
-                        result["ok"] = False
-                        result["error"] = msg
-                else:
-                    logger.info(
-                        "[{}] Issue key not found".format(log_entry["id"])
-                    )
-
-            db.put_log_entry(log_entry["id"])
+            handle_log_entry(log_entry)
 
     # anyway put last timestamp the the db at the end of last issues polling
     db.update_polling_timestamp(processing_timestamp)
